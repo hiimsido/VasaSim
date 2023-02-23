f1aa2b318709ad2690919324d348bf81b665627a
hiimsido <contacthiimsido@gmail.com>
2023-02-23 14:13:26 -0500

    hisoka's mama

--- src/main/java/org/example/ClawKillOdds.java
+++ src/main/java/org/example/ClawKillOdds.java
@@ -21,52 +21,66 @@ public class ClawKillOdds {
     public static class VasaStats
     {
 
-        boolean AtkRole = (.8351>Math.random());
-        boolean AtkRoleB = (.8448>Math.random());
 
-        int DmgA = (int)(Math.random()*(51+1));
-        int DmgB = (int)(Math.random()*(49+1));
 
 
         public static void DragonClaws(int Torva, int Inq) {
 
             ClawKillOdds Claw = new ClawKillOdds();
-            for (int d = 0; d < 2; d++) {
+            int TotalClawDamage = 0;
 
 
-                for (int i = 0; i < Torva; i++) {
+
+                for (int i = 0; i < 5; i++) {
 
 
                     int max = 43;
                     double maxTwo = 43.000;
                     double acc = .4217;
-                    boolean initiate = false;
-                    for (int j = 0; ((j < 4) && (!initiate)); j++) {
+
+
+                    if(Torva>0){
+                        Torva--;
+
+                    }
+                    else{
+                        Inq--;
+                        max = 41;
+                        acc = .3997;
+                        maxTwo = 41.000;
+                    }
+
+
+                    for (int j = 0; j < 4; j++) {
                         if ((j == 0) && Claw.Odds(acc)) {
                             int TotalSpecDMG = (int) (Math.random() * (max * .5)) + (max / 2);
-                            VasaCrystalHealth = VasaCrystalHealth - (TotalSpecDMG + (TotalSpecDMG / 2) + (TotalSpecDMG / 4) + (TotalSpecDMG / 4));
-                            initiate = true;
+                            TotalClawDamage = (TotalSpecDMG + (TotalSpecDMG / 2) + (TotalSpecDMG / 4) + (TotalSpecDMG / 4));
+                            VasaCrystalHealth = VasaCrystalHealth - TotalClawDamage;
+                            break;
 
                         }
 
                         else if ((j == 1) && Claw.Odds(acc)) {
                             int TotalSpecDMG = (int) (Math.random() * (maxTwo * .5)) + (int) (maxTwo * (.375));
-                            VasaCrystalHealth = VasaCrystalHealth - ((TotalSpecDMG ) + (TotalSpecDMG / 2) + (TotalSpecDMG / 2));
-                            initiate = true;
+                            TotalClawDamage = ((TotalSpecDMG ) + (TotalSpecDMG / 2) + (TotalSpecDMG / 2));
+                            VasaCrystalHealth = VasaCrystalHealth - TotalClawDamage;
+                            break;
 
                         }
 
                         else if ((j == 2) && Claw.Odds(acc)) {
                             int TotalSpecDMG = (int) (Math.random() * (maxTwo * .5)) + (int) (maxTwo * .25);
-                            VasaCrystalHealth = VasaCrystalHealth - (TotalSpecDMG * 2);
-                            initiate = true;
+                            TotalClawDamage = (TotalSpecDMG * 2);
+                            VasaCrystalHealth = VasaCrystalHealth - TotalClawDamage;
+                            break;
 
                         }
 
                         else if ((j == 3) && Claw.Odds(acc)) {
                             int TotalSpecDMG = (int) (Math.random() * (int) maxTwo ) + (int)(maxTwo / 4);
-                            VasaCrystalHealth = VasaCrystalHealth - TotalSpecDMG;
-                            initiate = true;
+                            TotalClawDamage = TotalSpecDMG;
+                            VasaCrystalHealth = VasaCrystalHealth - TotalClawDamage;
+                            break;
 
                         }
                     }
@@ -74,48 +88,11 @@ public class ClawKillOdds {
 
                 }
 
-                for (int i = 0; i < Inq; i++) {
 
-                    int max = 41;
-                    double acc = .3997;
-                    double maxTwo = 41.000;
-                    boolean initiate = false;
-                    for (int j = 0; ((j < 4) && (!initiate)); j++) {
-                        if ((j == 0) && Claw.Odds(acc)) {
-                            int TotalSpecDMG = (int) (Math.random() * max - (max / 2)) + (max / 2);
-                            VasaCrystalHealth = VasaCrystalHealth - (TotalSpecDMG + (TotalSpecDMG / 2) + (TotalSpecDMG / 4) + (TotalSpecDMG / 4));
-                            initiate = true;
-
-                        }
-
-                        else if ((j == 1) && Claw.Odds(acc)) {
-                            int TotalSpecDMG = (int) (Math.random() * (int) (maxTwo * (.875)) - (int) (maxTwo * (.375))) + (int) (maxTwo * (.375));
-                            VasaCrystalHealth = VasaCrystalHealth - (0 + (TotalSpecDMG / 2) + (TotalSpecDMG / 4) + (TotalSpecDMG / 4));
-                            initiate = true;
-
-                        }
-
-                        else if ((j == 2) && Claw.Odds(acc)) {
-                            int TotalSpecDMG = (int) (Math.random() * (int) ((maxTwo * (.75)) - (int) (maxTwo / 4))) + (int) (maxTwo / 4);
-                            VasaCrystalHealth = VasaCrystalHealth - (TotalSpecDMG + TotalSpecDMG);
-                            initiate = true;
-
-                        }
-
-                        else if ((j == 3) && Claw.Odds(acc)) {
-                            int TotalSpecDMG = (int) (Math.random() * (int) (maxTwo * 1.25) - (int) (maxTwo / 4)) + (int) (maxTwo / 4);
-                            VasaCrystalHealth = VasaCrystalHealth - TotalSpecDMG;
-                            initiate = true;
-
-                        }
-                    }
-
-
-                }
 
             }
         }
-    }
+
 
 
     public static void main(String[] args) {
@@ -129,12 +106,12 @@ public class ClawKillOdds {
 
 
 
+            for (int j = 0; j < 2; j++) {
+                VasaStats.DragonClaws(3,2);
+            }
 
 
-
-            ClawKillOdds.VasaStats Stats = new ClawKillOdds.VasaStats();
-            VasaStats.DragonClaws(3,2);
-            if (VasaCrystalHealth>=0)
+            if (VasaCrystalHealth>0)
             {
                 Failure = Failure + 1;
             }
